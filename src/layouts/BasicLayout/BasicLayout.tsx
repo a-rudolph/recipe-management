7e9767e2e98ac9b21aeadc8131a340b8173cb4b8
a-rudolph <55548329+a-rudolph@users.noreply.github.com>
2021-11-29 12:41:30 -0500

    add bg-sm for mobile

--- src/layouts/BasicLayout/BasicLayout.tsx
+++ src/layouts/BasicLayout/BasicLayout.tsx
@@ -1,15 +1,29 @@
-import { BG_PATH } from '@constants/assets'
+import { BG_PATH, BG_SM_PATH } from '@constants/assets'
+import breakpoints from '@constants/responsive'
 import styled from 'styled-components'
 
 type BasicLayoutProps = {
   children: React.ReactNode
 }
 
+const BG_1 = '#082032'
+
 const StyledDiv = styled.div`
-  background: url(${BG_PATH});
-  background-size: cover;
   height: 100vh;
   width: 100vw;
+
+  background: url(${BG_SM_PATH});
+  background-size: cover;
+  background-color: ${BG_1};
+  background-repeat: no-repeat;
+  background-position-x: center;
+
+  @media screen and (min-width: ${breakpoints.sm}px) {
+    background: url(${BG_PATH});
+    background-color: ${BG_1};
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
 `
 
 export default function BasicLayout({ children }: BasicLayoutProps) {

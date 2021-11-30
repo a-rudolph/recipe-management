e58b90bba7a0ad31c3d7e35ca7dd1d1ceb792c59
a-rudolph <55548329+a-rudolph@users.noreply.github.com>
2021-11-30 11:26:05 -0500

    setup styled types

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -2,6 +2,23 @@ import swiss from '@theme-ui/preset-swiss'
 
 export const BRAND_NAME = 'whea·ti·ful·ly'
 
+// fonts too
+
+export const theme = {
+  colors: {
+    wheaty_1: '#F6BB63',
+    wheaty_2: '#FFE3B9', // more monoy than wheaty tbh
+    wheaty_3: '#B2A086', // more monoy than wheaty tbh
+    wheaty_4: '#867A69', // more monoy than wheaty tbh
+    mono_1: '#222528',
+    mono_2: '#484F55',
+    mono_3: '#F3EEED',
+    secondary_1: '#692C20',
+    bg_1: '#082032',
+    bg_2: '#1C405B',
+  },
+}
+
 const palette = {
   ...swiss.colors,
   primary: '#222528',
---
+++ src/typings/styled.d.ts
@@ -0,0 +1,18 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      wheaty_1: string
+      wheaty_2: string
+      wheaty_3: string
+      wheaty_4: string
+      mono_1: string
+      mono_2: string
+      mono_3: string
+      secondary_1: string
+      bg_1: string
+      bg_2: string
+    }
+  }
+}

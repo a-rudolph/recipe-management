daaf6d25dfaa4efbcc139bbabd0c828895ffbfdf
a-rudolph <55548329+a-rudolph@users.noreply.github.com>
2021-11-30 11:27:05 -0500

    replace theme ui theme with styled-c

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,23 @@
-/** @jsx jsx */
-import { BRAND_NAME, defaultTheme } from '@styles/themes'
+import { BRAND_NAME, theme } from '@styles/themes'
 import { TimeContextProvider } from '@hooks/useTimeContext'
-import { jsx, ThemeProvider } from 'theme-ui'
+import { ThemeProvider } from 'styled-components'
 import { BasicLayout } from 'layouts'
-import { useRouter } from 'next/router'
 import { AppProps } from 'next/dist/next-server/lib/router/router'
-import { useMemo } from 'react'
-import getAvailableRecipes from '@utils/getAvailableRecipes'
 import Head from 'next/head'
 import '@styles/index.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-  const { key } = router.query
-
-  const recipe = useMemo(() => {
-    const recipes = getAvailableRecipes()
-
-    return recipes.find((recipe) => recipe.key === key)
-  }, [key])
-
-  const pageTitle = recipe?.name
-
   return (
     <TimeContextProvider>
-      <ThemeProvider theme={defaultTheme}>
+      <ThemeProvider theme={theme}>
         <Head>
-          <title>{pageTitle || BRAND_NAME}</title>
+          <title>{BRAND_NAME}</title>
           <link rel='icon' href='/favicon.ico' />
         </Head>
-        <BasicLayout>Hello Bread makers</BasicLayout>
+        <BasicLayout>
+          Hello Bread makers
+          {/* <Component {...pageProps} /> */}
+        </BasicLayout>
       </ThemeProvider>
     </TimeContextProvider>
   )
--- src/layouts/BasicLayout/BasicLayout.tsx
+++ src/layouts/BasicLayout/BasicLayout.tsx
@@ -1,4 +1,6 @@
 import { BG_PATH, BG_SM_PATH } from '@constants/assets'
+import { BRAND_NAME } from '@styles/themes'
+import { Text } from '@components/atoms'
 import breakpoints from '@constants/responsive'
 import styled from 'styled-components'
 
@@ -6,26 +8,31 @@ type BasicLayoutProps = {
   children: React.ReactNode
 }
 
-const BG_1 = '#082032'
-
 const StyledDiv = styled.div`
   height: 100vh;
   width: 100vw;
 
-  background: url(${BG_SM_PATH});
+  background-image: url(${BG_SM_PATH});
   background-size: cover;
-  background-color: ${BG_1};
+  background-color: ${({ theme }) => theme.colors.bg_1};
   background-repeat: no-repeat;
   background-position-x: center;
 
   @media screen and (min-width: ${breakpoints.sm}px) {
-    background: url(${BG_PATH});
-    background-color: ${BG_1};
-    background-size: cover;
-    background-repeat: no-repeat;
+    background-image: url(${BG_PATH});
+    background-position-x: unset;
   }
 `
 
+const Logo = () => {
+  return <Text>{BRAND_NAME}</Text>
+}
+
 export default function BasicLayout({ children }: BasicLayoutProps) {
-  return <StyledDiv>{children}</StyledDiv>
+  return (
+    <StyledDiv>
+      <Logo />
+      <div>{children}</div>
+    </StyledDiv>
+  )
 }

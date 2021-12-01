3329d7ee3d01d5a559f585de6167eeb703a81bf7
a-rudolph <55548329+a-rudolph@users.noreply.github.com>
2021-11-30 21:53:25 -0500

    use current time in timercard

--- src/components/TimeCard/TimeCard.tsx
+++ src/components/TimeCard/TimeCard.tsx
@@ -1,5 +1,7 @@
 import { Card, Text } from '@components/atoms'
 import styled from 'styled-components'
+import { useState, useEffect } from 'react'
+import { padNumber } from '@utils/formatTime'
 
 const StyledCard = styled(Card)`
   display: flex;
@@ -11,14 +13,36 @@ const StyledCard = styled(Card)`
   width: 168px;
 `
 
+const useTime = () => {
+  const [now, setNow] = useState(() => new Date())
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setNow(() => new Date())
+    }, 1000)
+
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [])
+
+  const hours = padNumber(now.getHours())
+  const minutes = padNumber(now.getMinutes())
+  const seconds = padNumber(now.getSeconds())
+
+  return { hours, minutes, seconds }
+}
+
 export default function TimerCard() {
+  const { hours, minutes, seconds } = useTime()
+
   return (
     <StyledCard>
       <Text fs='48px' color='wheaty_1'>
-        0:00
+        {hours}:{minutes}
       </Text>
       <Text fs='32px' color='wheaty_1'>
-        :00
+        :{seconds}
       </Text>
     </StyledCard>
   )
--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -9,3 +9,9 @@ export default function formatTime(n: number): string {
 
   return `${h}h ${min}m`
 }
+
+export const padNumber = (n: number) => {
+  if (n < 10) return `0${n}`
+
+  return n
+}

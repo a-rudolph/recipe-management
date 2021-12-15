758d2713bb121e8f851bdcc37a727fe1ca778c31
a-rudolph <55548329+a-rudolph@users.noreply.github.com>
2021-12-14 19:42:30 -0500

    remove bad favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -39,19 +39,6 @@ class MyDocument extends Document {
             sizes='180x180'
             href='/icons/apple-icon-180x180.png'
           />
-
-          <link
-            rel='icon'
-            type='image/png'
-            sizes='32x32'
-            href='/icons/favicon-32x32.png'
-          />
-          <link
-            rel='icon'
-            type='image/png'
-            sizes='16x16'
-            href='/icons/favicon-16x16.png'
-          />
         </Head>
         <body>
           <Main />

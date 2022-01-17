f3dcf57f1ad6f67a902625402b20505155615920
a-rudolph <55548329+a-rudolph@users.noreply.github.com>
2022-01-16 21:24:20 -0500

    remove step type

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -19,13 +19,6 @@ type RecipeType = {
   ingredients: IngredientType[]
 }
 
-type Step = {
-  title: string
-  time: import('moment').Moment
-}
-
-type Steps = Array<Step>
-
 type IngredientType = {
   quantity: number
   unit: string

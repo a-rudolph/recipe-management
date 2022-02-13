3ec08130a9329ea365b3f9b3236121630746b3a3
a-rudolph <adam.rudolph@live.ca>
2022-02-13 11:22:03 -0500

    add vertical timeline line

--- src/components/DetailedTimeline/DetailedTimeline.tsx
+++ src/components/DetailedTimeline/DetailedTimeline.tsx
@@ -1,10 +1,31 @@
 import { Button, Row, Text } from '@components/atoms'
+import { Fragment } from 'react'
 import DetailIcon from '@components/icons/Detail'
 import responsive from '@constants/responsive'
 import styled from 'styled-components'
 
 const StyledDiv = styled.div`
   width: 100%;
+
+  .main-col {
+    position: relative;
+
+    .timeline-content {
+      * {
+        z-index: 1;
+      }
+    }
+
+    .vert-line {
+      position: absolute;
+      right: 32px;
+      top: 0px;
+      height: calc(100% - 16px);
+      width: 3px;
+      margin: 6px;
+      background: ${({ theme }) => theme.colors.secondary_1};
+    }
+  }
 
   .main-row {
     border-bottom: 1px solid ${({ theme }) => theme.colors.wheaty_1};
@@ -20,16 +41,16 @@ const StyledDiv = styled.div`
   }
 
   .dot {
-    height: 12px;
-    width: 12px;
-    border-radius: 50%;
-    background: ${({ theme }) => theme.colors.secondary_1};
-    border: 2px solid ${({ theme }) => theme.colors.wheaty_1};
+    height: 4px;
+    width: 4px;
     margin: 8px;
+    border-radius: 50%;
+    border: 6px solid ${({ theme }) => theme.colors.secondary_1};
+    background: ${({ theme }) => theme.colors.wheaty_1};
   }
 
   .time-oval {
-    margin-bottom: -1px;
+    margin-bottom: -2px;
     margin-right: -18px;
     padding: 4px 16px;
     border-radius: 60px;
@@ -87,37 +108,45 @@ const DetailedTimeline = (props: { recipe: RecipeType }) => {
         </Button>
         <Text.h0 style={{ marginBottom: '16px' }}>Schedule</Text.h0>
       </Row>
-      {steps.map((step) => (
-        <>
-          <Row className='main-row' style={{ justifyContent: 'space-between' }}>
-            <Text.h1 style={{ margin: 0 }} weight={600} color='wheaty_2'>
-              {step.title}
-            </Text.h1>
-            <div className='time-oval'>
-              <Text>{step.time}</Text>
-            </div>
-          </Row>
-          {step.break && (
-            <Row
-              className='break-row'
-              style={{ height: `${step.length * 3}rem` }}
-            >
-              <Text fs='18px' secondary>
-                {step.break}
-              </Text>
-              <Text
-                style={{ margin: '0 4px' }}
-                fs='18px'
-                secondary
-                color='wheaty_1'
+      <div className='main-col'>
+        <div className='vert-line' />
+        <div className='timeline-content'>
+          {steps.map((step, i) => (
+            <Fragment key={i}>
+              <Row
+                className='main-row'
+                style={{ justifyContent: 'space-between' }}
               >
-                {step.pause}
-              </Text>
-              <div className='dot' />
-            </Row>
-          )}
-        </>
-      ))}
+                <Text.h1 style={{ margin: 0 }} weight={600} color='wheaty_2'>
+                  {step.title}
+                </Text.h1>
+                <div className='time-oval'>
+                  <Text>{step.time}</Text>
+                </div>
+              </Row>
+              {step.break && (
+                <Row
+                  className='break-row'
+                  style={{ height: `${step.length * 3}rem` }}
+                >
+                  <Text fs='18px' secondary>
+                    {step.break}
+                  </Text>
+                  <Text
+                    style={{ margin: '0 4px' }}
+                    fs='18px'
+                    secondary
+                    color='wheaty_1'
+                  >
+                    {step.pause}
+                  </Text>
+                  <div className='dot' />
+                </Row>
+              )}
+            </Fragment>
+          ))}
+        </div>
+      </div>
     </StyledDiv>
   )
 }

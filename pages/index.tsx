a67736b0f44efa039af655cd35a33e840c0c5a00
= <adam.rudolph@live.ca>
2022-05-02 21:06:53 -0400

    new layout card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,33 @@
 import getAvailableRecipes from '@utils/getAvailableRecipes'
-import BasicLayout from '@layouts/BasicLayout'
+// import BasicLayout from '@layouts/BasicLayout'
 import RecipeList from '@components/RecipeList'
+import { Card } from '@components/atoms'
+import breakpoints from '@constants/breakpoints'
+import styled from 'styled-components'
 
 type HomeProps = {
   recipes: RecipeType[]
 }
 
+const LayoutCard = styled(Card)`
+  width: 80vw;
+  min-height: 60vh;
+  margin-bottom: 24px;
+  padding: 24px;
+  overflow: hidden;
+
+  @media screen and (min-width: ${breakpoints.sm}px) {
+    width: 320px;
+    border-radius: 4px;
+  }
+`
+
 const Home = ({ recipes }: HomeProps) => {
   return (
-    <BasicLayout.Card side='right'>
+    <LayoutCard side='right'>
       recipe LIST
       {/* <RecipeList recipes={recipes} /> */}
-    </BasicLayout.Card>
+    </LayoutCard>
   )
 }
 

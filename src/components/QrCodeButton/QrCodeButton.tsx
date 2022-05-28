f650d236b307246333ae3f546915af02f3a3cb8f
= <adam.rudolph@live.ca>
2022-05-27 20:15:55 -0400

    generate qrcode with library

--- src/components/QrCodeButton/QrCodeButton.tsx
+++ src/components/QrCodeButton/QrCodeButton.tsx
@@ -1,27 +1,54 @@
 import { Text, Button } from '@components/atoms'
-import { Row, Col, Modal } from 'antd'
+import { Row, Col, Dropdown } from 'antd'
+import styled, { useTheme } from 'styled-components'
+import QRCode from 'qrcode'
 import { QrcodeOutlined } from '@ant-design/icons'
 
+const CANVAS_ID = 'canvas-id'
+
+const StyledCanvas = styled.canvas`
+  border-radius: 8px;
+  border: 1px solid ${({ theme }) => theme.colors.secondary_1};
+  box-shadow: ${({ theme }) => theme.shade.small};
+  background: ${({ theme }) => theme.colors.primary_1};
+`
+
 const QrCodeButton = () => {
-  const onClick = () => {
-    Modal.info({
-      content: <img alt='qr-code' src='/qr-codes/qr-development.png' />,
-      maskClosable: true,
-      icon: false,
+  const theme = useTheme()
+
+  const onVisibleChange = (isVisible: boolean) => {
+    if (!isVisible) return
+
+    const url = location.href
+    const canvas = document.getElementById(CANVAS_ID)
+
+    QRCode.toCanvas(canvas, url, {
+      width: 300,
+      color: {
+        dark: theme.colors.wheaty_1,
+        light: theme.colors.primary_1,
+      },
     })
   }
 
   return (
-    <Button type='ghost' onClick={onClick}>
-      <Text>
-        <Row gutter={8}>
-          <Col>
-            <QrcodeOutlined />
-          </Col>
-          <Col>open on your phone</Col>
-        </Row>
-      </Text>
-    </Button>
+    <Dropdown
+      forceRender={true}
+      onVisibleChange={onVisibleChange}
+      trigger={['click']}
+      overlay={<StyledCanvas id={CANVAS_ID} />}
+    >
+      <Button type='ghost'>
+        <Text>
+          <Row gutter={8}>
+            <Col>
+              <QrcodeOutlined />
+            </Col>
+            <Col>open on your phone</Col>
+          </Row>
+        </Text>
+      </Button>
+    </Dropdown>
   )
 }
 

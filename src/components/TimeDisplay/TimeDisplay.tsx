993e63d50b27db90d635b3f8dcea455eb14da14f
a-rudolph <adam.rudolph@live.ca>
2023-03-01 19:05:27 -0500

    remove unused styled component

--- src/components/TimeDisplay/TimeDisplay.tsx
+++ src/components/TimeDisplay/TimeDisplay.tsx
@@ -1,8 +1,5 @@
-import styled, { CSSProperties } from 'styled-components'
-import { MutableRefObject } from 'react'
+import type { CSSProperties, MutableRefObject } from 'react'
 import { Text } from '@components/atoms'
-
-const StyledTime = styled.div``
 
 type TimeDisplayProps = {
   hmRef: MutableRefObject<HTMLSpanElement | null>
@@ -15,15 +12,13 @@ export default function TimeDisplay(props: TimeDisplayProps) {
   const { hmRef, ssRef, ...rest } = props
 
   return (
-    <StyledTime className='time-container' {...rest}>
+    <div className='time-container' {...rest}>
       <Text fs='h1' color='wheaty_1'>
         <span ref={hmRef}>00:00</span>
       </Text>
       <Text fs='h1' color='wheaty_1'>
         <span ref={ssRef}>:00</span>
       </Text>
-    </StyledTime>
+    </div>
   )
 }
-
-/** trim zeros */
